fc839bde53c0cc34d83aa7124020bcbc2d1e06a0
thomasyoung66 <65619882@qq.com>
2017-09-01 16:51:38 +0800

    增加狼

--- src/utils/util.js
+++ src/utils/util.js
@@ -15,10 +15,42 @@ function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
+function alert(str)
+{
+  wx.showModal({
+    title: '弹窗标题',
+    content: str,
+    confirmText: "主操作",
+ //   cancelText: "辅助操作",
+    success: function (res) {
+      console.log(res);
+      if (res.confirm) {
+        console.log('用户点击主操作')
+      } else {
+        console.log('用户点击辅助操作')
+      }
+    }
+  });
+}
 function test(){
   return "i come from utils modules!";
 }
+function dump_obj(obj){
+  for(var o in obj){
+    console.log(o+":"+obj[o]);
+  }
+}
+function toast(t)
+{
+  wx.showToast({
+    title: t,
+  });
+}
+
 module.exports = {
   formatTime: formatTime,
-  test:test
+  test:test,
+  alert:alert,
+  dump_obj:dump_obj,
+  toast: toast
 }
