28b68779da9174578088105cbd04c19f59945195
thomasyoung66 <65619882@qq.com>
2017-09-07 11:22:06 +0800

    提交代码

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,6 +2,8 @@
 //获取应用实例
 var util = require('../../utils/util.js');
 var ble = require('../../utils/ble_api.js');
+var Charts = require('../../utils/wxcharts.js');
+var debug_ui=true;
 
 var app = getApp()
 Page({
@@ -9,8 +11,19 @@ Page({
 	data: {
 		motto: 'index',
 		userInfo: {},
-		isConnect: 0
-
+		currDateShow:'',
+		isConnect: 0,
+		step_total:1600,// 总的步数
+		step_ps:40,//步数的百分比
+		step_dest:7000,//步数目标
+		step_dist:1314, //距离
+		step_cal:183,
+		step_time:"10:00:00",
+		sleep_total:"3.5", 
+		good_sleep_time:3.5,
+		bad_sleep_time:2.0,
+		sober_sleep_time:1.0,
+		end:0
 	},
 	showDetail: function (data) {
 		console.log("this is ok.." + data);
@@ -67,22 +80,119 @@ Page({
 			});
 		}
 	},
+	prepDateTap:function(){
+		console.log("prep date");
+	},
+	nextDateTap: function () {
+		console.log("next date");
+	},
+	bindDateChange: function (e) {
+		this.setData({
+			currDateShow: e.detail.value
+		})
+	},  
+	drawHeartRateCanvas: function () {
+		new Charts({
+			canvasId: 'heartrateCanvas',
+			type: 'column',
+			categories: ['0:00', '', '', '',
+				'4:00', '', '', '',
+				'8:00', '', '', '',
+				'12:00', '', '', '',
+				'16:00', '', '', '',
+				'20:00', '', '', '',
+				'24:00'],
+			series: [{
+				name: '时间段步数详细信息',
+				data: [0, 0, 4000, 8000, 4, 13000]
+			}],
+			yAxis: {
+				format: function (val) {
+					return val + '步';
+				}
+			},
+			width: 360,
+			height: 200,
+			dataLabel: false
+		});
+		return;
+	},
+	drawStepCanvas:function(){
+		new Charts({
+			canvasId: 'setpCanvas',
+			type: 'column',
+			categories: ['0:00', '', '', '', 
+					'4:00',  '', '', '', 
+					'8:00', '', '', '', 
+				'12:00', '', '', '',
+				'16:00', '', '', '', 
+				'20:00', '', '', '',
+				'24:00'],
+			series: [{
+				name: '时间段步数详细信息',
+				data: [0, 0, 4000, 8000, 4, 13000]
+			}],
+			yAxis: {
+				format: function (val) {
+					return val + '步';
+				}
+			},
+			width: 360,
+			height: 200,
+			dataLabel: false
+		});
+		return ;
+	},
+	drawSleepCanvas: function () {
+		new Charts({
+			canvasId: 'sleepCanvas',
+			type: 'column',
+			categories: ['20:00', '', '', '',
+				'0:00', '', '', '',
+				'4:00', '', '', '',
+				'8:00', '', '', '',
+				'12:00', '', '', '',
+				'16:00'],
+			series: [{
+				name: '翻身数量统计',
+				data: [0, 0, 4000, 8000, 4, 13000]
+			}],
+			yAxis: {
+				format: function (val) {
+					return val + '步';
+				}
+			},
+			width: 360,
+			height: 200,
+			dataLabel: false
+		});
+		return;
+	},
 	onLoad: function () {
 		console.log('onLoad');
 
 		var that = this;
+		var now = new Date();
+		that.setData({
+			currDateShow: util.sprintf("%d-%02d-%02d", now.getFullYear(), now.getMonth() + 1, now.getDate())
+		});
 		console.log("getTimeDiff=" + this.getTimeDiff());
-		ble.init(0);
-		wx.getStorage({
-			key: 'curr_device_id',
-			success: function (res) {
-				console.log(res);
-				ble.run(res.data);
-			}
-		})
+		if (debug_ui==false){
+			ble.init(0);
+			wx.getStorage({
+				key: 'curr_device_id',
+				success: function (res) {
+					console.log(res);
+					ble.run(res.data);
+				}
+			});
+			setInterval(this.onTimer, 1000);
+		}
 
-		setInterval(this.onTimer, 1000);
-
+	
+		this.drawStepCanvas();
+		this.drawSleepCanvas();
+		this.drawHeartRateCanvas();
 		/*
 		  wx.getLocation({
 			type: 'wgs84',
